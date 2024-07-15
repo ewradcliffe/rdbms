1a42ca995853a449769d717162740083512aa934
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-07-15 17:17:43 +0000

    Update routing code.

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,8 +10,7 @@ def home():
 
 @app.route("/categories")
 def categories():
-    categories = list(Category.query.order_by(Category.category_name).all())
-    return render_template("categories.html", categories=categories)
+    return render_template("categories.html")
 
 
 @app.route("/add_category", methods=["GET", "POST"])
