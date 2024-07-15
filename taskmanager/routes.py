96ca76f1b9fc6f3679946f1d4126b72367281a40
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-07-15 16:14:39 +0000

    Add code to query database from site.

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,15 +1,24 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
-from taskmanager.models import Catagory, Task
+from taskmanager.models import Category, Task
+
 
 @app.route("/")
 def home():
     return render_template("tasks.html")
 
-@app.route("/catagories")
-def catagories():
-    return render_template("catagories.html")
+
+@app.route("/categories")
+def categories():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
+
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
-    return render_template("add_catagory.html")
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
